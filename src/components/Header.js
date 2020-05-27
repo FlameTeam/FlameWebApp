912a31e5b5c28a0eb884f44fe193dc75b4e8ae50
Dixon <you@example.com>
2020-05-26 23:40:45 -0400

    grid layout advanced

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,14 +11,24 @@ export default function Navbar() {
 
   return (
         <Grid container className={classes.header}>
-            <Grid item md={4} className={classes.imageGrid}>
+            <Grid item xs={12} sm={4} md={5} className={classes.imageGrid}>
                 <img alt="girl-in-desk" src={GirlInDeskImage} className={classes.image}/>
             </Grid>
 
-            <Grid item md={8}>
+            <Grid item xs={12} sm={8} md={7}>
                 <h2 className={classes.title}>Soluciones de Software hechas por un equipo!</h2>
-                
-                <Box ></Box>
+                <Grid container className={classes.gridBoxesContainer} >
+                    <Grid item xs={12} sm={5} className={classes.gridItemBox}>
+                        <Box>
+                            CAJA 1
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={5} className={classes.gridItemBox}>
+                        <Box>
+                            CAJA 2  
+                        </Box>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     
--- src/styles/components/headerStyle.js
+++ src/styles/components/headerStyle.js
@@ -4,15 +4,32 @@ const useStyles = makeStyles((theme) => ({
   header: {
     background: "linear-gradient(6.53deg, #EB3333 11.2%, #F45C43 88.07%)",
     flexGrow: "1",
+    alignItem: "center",
     height: "400px",
-    border: "1px solid black",
-    margin: "0"
+    
+    margin: "0",
+
+    [theme.breakpoints.down("xs")]: {
+      height: "900px",
+    },
+   
   },
 
   imageGrid: {
-    borderRight: "1px solid white",
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
+    marginBottom: "0",
+    border: "1px solid white"
+  },
+
+  image: {
+    marginBottom: "0",
+    [theme.breakpoints.up("sm")]: {
+      height: "340px",
+    },
+    [theme.breakpoints.up("md")]: {
+      height: "375px",
+    }
   },
 
   title: {
@@ -22,10 +39,27 @@ const useStyles = makeStyles((theme) => ({
       marginTop: "0.5em",
       textAlign: "center",
 
-
-      border: "1px solid blue",
+      [theme.breakpoints.down("sm")]: {
+        fontSize: "1em",
+      },
+      [theme.breakpoints.down("md")]: {
+        fontSize: "2em",
+      },
       
 
+      // border: "1px solid blue",
+  },
+
+  gridBoxesContainer: {
+    display: "flex",
+    justifyContent: "space-around"
+  },
+
+  gridItemBox: {
+    border: "1px solid white",
+    margin: "0.5em",
+    height: "200px",
+    width: "40%"
   }
 }));
 
