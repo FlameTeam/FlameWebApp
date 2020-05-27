0432cd6916d420afa787a8f6adf9e3c5e42a41a1
Dixon <you@example.com>
2020-05-27 02:31:35 -0400

    header full responsive, boxes with button waiting for develop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-// import React, { useContext } from "react";
 import React, { useState } from "react";
 
 import {
--- src/styles/components/headerStyle.js
+++ src/styles/components/headerStyle.js
@@ -2,28 +2,26 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   header: {
-    background: "linear-gradient(6.53deg, #EB3333 11.2%, #F45C43 88.07%)",
+    background: "linear-gradient(0deg, #D85656 0%, #F45C43 100%)",
     flexGrow: "1",
     alignItem: "center",
-    height: "400px",
     
-    margin: "0",
-
-    [theme.breakpoints.down("xs")]: {
-      height: "900px",
-    },
    
   },
 
   imageGrid: {
+    
+    margin: "0",
     display: "flex",
     justifyContent: "center",
-    marginBottom: "0",
-    border: "1px solid white"
+    
   },
 
   image: {
-    marginBottom: "0",
+    
+    [theme.breakpoints.up("xs")]: {
+      height: "300px",
+    },
     [theme.breakpoints.up("sm")]: {
       height: "340px",
     },
@@ -36,18 +34,17 @@ const useStyles = makeStyles((theme) => ({
       color: "#FFFFFF",
       fontWeight: "normal",
       fontSize: "2.5em",
-      marginTop: "0.5em",
+      marginTop: "0",
       textAlign: "center",
 
-      [theme.breakpoints.down("sm")]: {
-        fontSize: "1em",
+      [theme.breakpoints.up("xs")]: {
+        fontSize: "2.3em",
+        paddingLeft: "1em",
+        paddingRight: "1em"
       },
-      [theme.breakpoints.down("md")]: {
-        fontSize: "2em",
-      },
-      
-
-      // border: "1px solid blue",
+      [theme.breakpoints.up("sm")]: {
+        fontSize: "2.4em",
+      }
   },
 
   gridBoxesContainer: {
@@ -57,7 +54,6 @@ const useStyles = makeStyles((theme) => ({
 
   gridItemBox: {
     border: "1px solid white",
-    margin: "0.5em",
     height: "200px",
     width: "40%"
   }
