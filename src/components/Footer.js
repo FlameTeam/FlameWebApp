27dcfd62b0550ea1c9fae939c4fdefa373ff486f
DixonOrtiz <dixon.ortizc@gmail.com>
2020-09-07 19:30:19 -0300

    footer icons deleted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import useCustomStyles from '../resources/styles/customStyle';
 import columnStyles from '../resources/styles/subcomponents/footer/columnStyle';
 import { Grid, Typography } from '@material-ui/core';
-import MailIcon from '@material-ui/icons/Mail';
-import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
-import BusinessIcon from '@material-ui/icons/Business';
 
 import Column from './subcomponents/columns/Column';
 import Errors from './subcomponents/Errors';
 
 import { footerData } from '../resources/data/footerData';
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    color: '#ffffff',
-    marginRight: '-18px',
-  },
-}));
-
 export function Footer({ error }) {
   const customClasses = useCustomStyles();
   const columnsClasses = columnStyles();
-  const classes = useStyles();
 
   if (error) {
     return <Errors type={error} />;
@@ -44,19 +32,16 @@ export function Footer({ error }) {
             </Grid>
             <hr className={columnsClasses.line} />
             <Grid container justify='center'>
-              <PhoneAndroidIcon className={classes.icon} />
               <Typography className={columnsClasses.finalColumnFooter}>
                 (9) 5733 2640
               </Typography>
             </Grid>
             <Grid container justify='center'>
-              <MailIcon className={classes.icon} />
               <Typography className={columnsClasses.finalColumnFooter}>
                 comercial@flamedev.cl
               </Typography>
             </Grid>
             <Grid container justify='center'>
-              <BusinessIcon className={classes.icon} />
               <Typography className={columnsClasses.finalColumnFooter}>
                 Huilliches 673, Quilicura, Chile
               </Typography>
